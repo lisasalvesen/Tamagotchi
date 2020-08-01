538b0ce4fb8534556bee2f5a9cfb6a7767b12d20
Lisa Salvesen <salvesen@fim.uni-passau.de>
2020-08-01 19:21:55 +0200

    initial application files

---
+++ src/main/java/spe_assignment3/tamagotchi/TamagotchiApplication.java
@@ -0,0 +1,13 @@
+package spe_assignment3.tamagotchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagotchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamagotchiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spe_assignment3/tamagotchi/TamagotchiApplicationTests.java
@@ -0,0 +1,11 @@
+package spe_assignment3.tamagotchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class TamagotchiApplicationTests {
+
+}
